cbb05a4b07a1c53c2cd36ca95b9f35323b8486ad
Ye Hua Tan <yh2tan@uwaterloo.ca>
2020-04-09 16:36:34 -0400

    test commit

--- DFODamageBuilder/script.js
+++ DFODamageBuilder/script.js
@@ -1,5 +1,4 @@
 import { Modifiers, BaseAttribute } from './models/stats.js'
 
 var mod = new Modifiers()
-var json_data = fetch("./data/epic_95/tb_cloth.json")
-    .then(response => response.json()).then(arr => console.log(arr))
+fetch("./data/epic_95/tb_cloth.json").then(response => response.json()).then(arr => mod.get_data(arr[0]))
